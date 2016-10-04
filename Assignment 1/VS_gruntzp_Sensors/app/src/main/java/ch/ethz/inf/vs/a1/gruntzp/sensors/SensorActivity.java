9b6ffb75be9560897465f8454565dffb02dbabec
p.gruntz <patrick.gruntz@web.de>
2016-10-04 18:15:00 +0200

    Add interface SensorType
    TODO: graphics

--- Assignment 1/VS_gruntzp_Sensors/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/sensors/SensorActivity.java
+++ Assignment 1/VS_gruntzp_Sensors/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/sensors/SensorActivity.java
@@ -1,10 +1,12 @@
 package ch.ethz.inf.vs.a1.gruntzp.sensors;
 
 import android.content.Context;
+import android.content.SyncStatusObserver;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,6 +20,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
     private Sensor currentSensor;
     private TextView tv;
     private String sensorName;
+    private SensorTypesImpl sensorType = new SensorTypesImpl();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +40,7 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
 
         int[] const_sensors = new int[] {Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_AMBIENT_TEMPERATURE, Sensor.TYPE_GRAVITY,
                                         Sensor.TYPE_GYROSCOPE, Sensor.TYPE_LIGHT, Sensor.TYPE_MAGNETIC_FIELD, Sensor.TYPE_PRESSURE,
-                                        Sensor.TYPE_PROXIMITY, Sensor.TYPE_RELATIVE_HUMIDITY, Sensor.TYPE_ROTATION_VECTOR};
+                                        Sensor.TYPE_PROXIMITY, Sensor.TYPE_RELATIVE_HUMIDITY};
         assert (position < const_sensors.length);
         currentSensor = sensorM.getDefaultSensor(const_sensors[position]);
 
@@ -48,8 +51,11 @@ public class SensorActivity extends AppCompatActivity implements SensorEventList
         //float val = event.values[0];
 
         //tv.setText(sensorName + ": " + Float.toString(val));
-        tv.setText(sensorName + ": " + Arrays.toString(event.values));
+        int size = sensorType.getNumberValues(currentSensor.getType());
+        String unit = sensorType.getUnitString(currentSensor.getType());
 
+        //tv.setText(sensorName + ": " + Arrays.toString(event.values));
+        tv.setText("Number Of Values: " + Integer.toString(size) + "\n" + Arrays.toString(event.values) + unit);
 
     }
 
---
+++ Assignment 1/VS_gruntzp_Sensors/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/sensors/SensorTypesImpl.java
@@ -0,0 +1,45 @@
+package ch.ethz.inf.vs.a1.gruntzp.sensors;
+
+import android.hardware.Sensor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SensorTypesImpl implements SensorTypes {
+
+    private int[] threes = new int[] {Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_GRAVITY, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_MAGNETIC_FIELD};
+
+    public int getNumberValues(int sensorType){
+
+        if(sensorType == Sensor.TYPE_ACCELEROMETER || sensorType == Sensor.TYPE_GRAVITY || sensorType == Sensor.TYPE_MAGNETIC_FIELD || sensorType == Sensor.TYPE_GYROSCOPE) {
+             return 3;
+         }else{
+             return 1;
+        }
+    }
+
+    public String getUnitString(int sensorType){
+        switch(sensorType){
+            case Sensor.TYPE_ACCELEROMETER:
+                return "m/s^2";
+            case Sensor.TYPE_AMBIENT_TEMPERATURE:
+                return "°C";
+            case Sensor.TYPE_GRAVITY:
+                return "m/s^2";
+            case Sensor.TYPE_GYROSCOPE:
+                return "rad/s";
+            case Sensor.TYPE_LIGHT:
+                return "lx";
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                return "µT";
+            case Sensor.TYPE_PRESSURE:
+                return "hPa";
+            case Sensor.TYPE_PROXIMITY:
+                return "cm";
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                return "%";
+            default:
+                return null;
+        }
+    }
+}
