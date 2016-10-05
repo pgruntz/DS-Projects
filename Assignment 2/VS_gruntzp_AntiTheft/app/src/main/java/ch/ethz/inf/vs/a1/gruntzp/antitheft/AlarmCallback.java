eb26a76ab191c28862436caa794e47153dac8c24
Philipp Schimmelfennig <phisch@outlook.de>
2016-10-05 15:39:36 +0200

    Added Task 2

---
+++ Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/AlarmCallback.java
@@ -0,0 +1,5 @@
+package ch.ethz.inf.vs.a1.gruntzp.antitheft;
+
+public interface AlarmCallback {
+    void onDelayStarted();
+}
---
+++ Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/MainActivity.java
@@ -0,0 +1,27 @@
+package ch.ethz.inf.vs.a1.gruntzp.antitheft;
+
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static NotificationManager nManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    public void onClickToggle(View v){
+        ToggleButton t = (ToggleButton) v;
+        if (t.isChecked()){
+            AlarmCallback ac = (AlarmCallback) new TheftService();
+            ac.onDelayStarted();
+        }
+    }
+}
