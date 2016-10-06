215b01b9a9288972201fd1d658f2ec2bb7f59016
Philipp Schimmelfennig <phisch@outlook.de>
2016-10-06 16:03:11 +0200

    nothing

--- Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/MainActivity.java
+++ Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/MainActivity.java
@@ -1,6 +1,9 @@
 package ch.ethz.inf.vs.a1.gruntzp.antitheft;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +25,37 @@ public class MainActivity extends AppCompatActivity {
         if (t.isChecked()){
             AlarmCallback ac = (AlarmCallback) new TheftService();
             ac.onDelayStarted();
+            Test();
         }
+    }
+
+    private void Test(){
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // Because clicking the notification opens a new ("special") activity, there's
+        // no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.fav57)
+                .setContentTitle("Theft Alarm!")
+                .setContentText("THEFT ALARM!");
+        //.setCategory(Notification.CATEGORY_ALARM)
+        //.setPriority(Notification.PRIORITY_MAX)
+        //.setVisibility(Notification.VISIBILITY_PUBLIC)
+        //.setVibrate(new long[] {100, 100, 100, 100});
+        notBuilder.setContentIntent(resultPendingIntent);
+        int mNotificationId = 001;
+
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, notBuilder.build());
     }
 }
--- Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/TheftService.java
+++ Assignment 2/VS_gruntzp_AntiTheft/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/antitheft/TheftService.java
@@ -2,6 +2,7 @@ package ch.ethz.inf.vs.a1.gruntzp.antitheft;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -41,6 +42,18 @@ public class TheftService extends Service implements AlarmCallback {
 
     @Override
     public void onDelayStarted() {
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// Because clicking the notification opens a new ("special") activity, there's
+// no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
         NotificationCompat.Builder notBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.fav57)
                 .setContentTitle("Theft Alarm!")
@@ -49,7 +62,12 @@ public class TheftService extends Service implements AlarmCallback {
         //.setPriority(Notification.PRIORITY_MAX)
         //.setVisibility(Notification.VISIBILITY_PUBLIC)
         //.setVibrate(new long[] {100, 100, 100, 100});
-        NotificationManager nManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        MainActivity.nManager.notify(1, notBuilder.build());
+        notBuilder.setContentIntent(resultPendingIntent);
+        int mNotificationId = 001;
+
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, notBuilder.build());
     }
 }
