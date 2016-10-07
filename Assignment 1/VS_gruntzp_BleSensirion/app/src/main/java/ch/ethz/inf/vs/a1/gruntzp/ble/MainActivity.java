5d906d0ea5f858b7e412a5e881fb3331a44cdc82
niederbm <madphon@hotmail.com>
2016-10-07 17:27:51 +0200

    Permissions sollten komplett funktionieren, scan findet das Gadget.

--- Assignment 1/VS_gruntzp_BleSensirion/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/ble/MainActivity.java
+++ Assignment 1/VS_gruntzp_BleSensirion/app/src/main/java/ch/ethz/inf/vs/a1/gruntzp/ble/MainActivity.java
@@ -2,7 +2,12 @@ package ch.ethz.inf.vs.a1.gruntzp.ble;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -14,7 +19,11 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,11 +37,18 @@ public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_LOC = 3;
 
     //Int to check wether location services weren't enabled and subsequently quit the app. Must not be static!
-    public int locDenied = 0;
+    public int locDenied;
 
     private boolean mScanning;
-    private Handler mHandler; //Needed for delayed cancel of BLE search
-    private static final long SCAN_PERIOD = 10000;
+    private final Handler mHandler = new Handler(); //Needed for delayed cancel of BLE search
+    private static final long SCAN_PERIOD = 80000;
+
+    //Will hold results of ble scan
+    private ArrayList<BluetoothDevice> listDevices;
+    private ArrayList<String> listDeviceNames;
+
+    //Used to dynamically update the listview of ble scan results
+    private static ArrayAdapter<String> mArrayAdapter;
 
 
 
@@ -43,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPackageMgr = getPackageManager();
+        locDenied = 0;
 
 
         //Used to check whether BLE is available, if not quit.
@@ -73,9 +90,12 @@ public class MainActivity extends AppCompatActivity {
         if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        if (mBluetoothAdapter.isEnabled() && !mLocationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        } else if (mBluetoothAdapter.isEnabled() && !mLocationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(this, R.string.get_loc, Toast.LENGTH_SHORT).show();
             getLocationServices();
+        } else {
+            //Toast.makeText(this, R.string.loc_enabled, Toast.LENGTH_SHORT).show();
+            scanLeDevice();
         }
 
     }
@@ -86,8 +106,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_ENABLE_BT) {
             // Check if bluetooth was indeed enabled
             if (resultCode == RESULT_OK) {
-                Toast.makeText(this, R.string.bluetooth_enabled, Toast.LENGTH_SHORT).show();
-                getLocationServices(); //Only request location servies if bluetooth is enabled
+                //Toast.makeText(this, R.string.bluetooth_enabled, Toast.LENGTH_SHORT).show();
             } else { //Bluetooth not enabled, end app
                 Toast.makeText(this, R.string.bluetooth_not_enabled, Toast.LENGTH_SHORT).show();
                 finish();
@@ -99,8 +118,7 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         if (requestCode == PERMISSION_REQUEST_FINE_LOC) {
             // If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { //Permission granted
-
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { //Permission granted, do nothing
 
             } else {
                 Toast.makeText(this, R.string.fine_loc_denied, Toast.LENGTH_SHORT).show();
@@ -111,45 +129,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getLocationServices() {
+        Intent enableLocIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(enableLocIntent);
+    }
 
-        if (!mLocationMgr.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if(locDenied == 1) { //Second time we're here, abort
-                Toast.makeText(this, R.string.loc_not_enabled, Toast.LENGTH_SHORT).show();
-                finish();
-                return;
-            } else {
-                ++locDenied;
+    private void scanLeDevice() {
+
+        listDeviceNames = new ArrayList<String>();
+        listDevices = new ArrayList<BluetoothDevice>();
+
+        mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listDeviceNames);
+
+        ListView listView = (ListView) findViewById(R.id.listDeviceNames);
+        listView.setAdapter(mArrayAdapter);
+
+
+        //Implement a scan filter to only look for Sensirion HumiGadget devices
+        ScanFilter.Builder mScanFilterBuilder = new ScanFilter.Builder();
+        ScanFilter mScanFilter = mScanFilterBuilder.build();
+
+        final BluetoothLeScanner mBleScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+
+        // Stops scanning after a pre-defined scan period.
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScanning = false;
+                mBleScanner.stopScan(mScanCallback);
+                Toast.makeText(MainActivity.super.getApplicationContext(), R.string.stop_scan, Toast.LENGTH_SHORT).show();
             }
-            Intent enableLocIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(enableLocIntent);
-        } else {//Location services enabled!
-            Toast.makeText(this, R.string.loc_enabled, Toast.LENGTH_SHORT).show();
-            locDenied = 2;
-            //Initiate search for BLE devices
+        }, SCAN_PERIOD);
 
-        }
+        mScanning = true;
+        mBleScanner.startScan(mScanCallback);
+        Toast.makeText(this, R.string.start_scan, Toast.LENGTH_SHORT).show();
+
 
     }
 
-    private void scanLeDevice(final boolean enable) {
-        if (enable) {
-            // Stops scanning after a pre-defined scan period.
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mScanning = false;
-                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+    final ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            Toast.makeText(MainActivity.super.getApplicationContext(), R.string.found_device, Toast.LENGTH_SHORT).show();
+            int index;
+            boolean deviceAdded;
+            if (callbackType == 4) { //Previously matched device lost
+                index = listDevices.indexOf(result.getDevice());
+                if(!(index == -1)) { //Device has to be removed
+                    listDevices.remove(index);
+                    listDeviceNames.remove(index);
                 }
-            }, SCAN_PERIOD);
-
-            mScanning = true;
-            mBluetoothAdapter.startLeScan(mLeScanCallback);
-        } else {
-            mScanning = false;
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            } else { //New device found
+                deviceAdded = listDevices.add(result.getDevice());
+                if (deviceAdded) {
+                    listDeviceNames.add(listDevices.get(listDevices.size()-1).getName());
+                    mArrayAdapter.notifyDataSetChanged();
+                }
+            }
         }
-        
-    }
+    };
+
+
 
 
 
