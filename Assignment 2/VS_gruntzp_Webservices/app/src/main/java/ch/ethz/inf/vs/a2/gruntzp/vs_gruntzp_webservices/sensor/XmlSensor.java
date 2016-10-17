f1d298638d904ea317be84b5d12f0e81fb9f0cb7
Philipp Schimmelfennig <phisch@outlook.de>
2016-10-17 16:44:19 +0200

    irgendwas noch

---
+++ Assignment 2/VS_gruntzp_Webservices/app/src/main/java/ch/ethz/inf/vs/a2/gruntzp/vs_gruntzp_webservices/sensor/XmlSensor.java
@@ -0,0 +1,20 @@
+package ch.ethz.inf.vs.a2.gruntzp.vs_gruntzp_webservices.sensor;
+
+import ch.ethz.inf.vs.a2.sensor.AbstractSensor;
+
+/**
+ * Created by PhiSc on 16.10.2016.
+ */
+
+public class XmlSensor extends AbstractSensor {
+    @Override
+    public String executeRequest() throws Exception {
+
+        return null;
+    }
+
+    @Override
+    public double parseResponse(String response) {
+        return 0;
+    }
+}
