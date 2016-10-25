cd405cbd06a96aa1f2598157b98c8308e632f6d9
Philipp Schimmelfennig <phisch@outlook.de>
2016-10-25 15:58:43 +0200

    init2

---
+++ Assignment 3/vs-gruntzp-chat/app/src/main/java/ch/ethz/inf/vs/a3/message/MessageComparator.java
@@ -0,0 +1,17 @@
+package ch.ethz.inf.vs.a3.message;
+
+import java.util.Comparator;
+//import ch.ethz.inf.vs.a3.message.Message;
+
+/**
+ * Message comparator class. Use with PriorityQueue.*/
+//public class MessageComparator implements Comparator<Message> {
+//
+//    @Override
+//    public int compare(Message lhs, Message rhs) {
+//        // Write your code here
+//
+//        return 0;
+//    }
+//
+//}
