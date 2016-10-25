2a608c5f58485179afe1fb58249d617111a5cba0
p.gruntz <patrick.gruntz@web.de>
2016-10-25 17:09:48 +0200

    LamportClocks

--- Assignment 3/vs-gruntzp-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/LamportClock.java
+++ Assignment 3/vs-gruntzp-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/LamportClock.java
@@ -8,9 +8,7 @@ public class LamportClock implements Clock {
 
     private int time;
 
-    void setTime(int time){
-        this.time=time;
-    }
+    void setTime(int time){this.time=time;}
 
     int getTime() {
         return time;
@@ -18,12 +16,13 @@ public class LamportClock implements Clock {
 
     @Override
     public void update(Clock other) {
-
+        if(((LamportClock) other).getTime()>time)
+            time = ((LamportClock) other).getTime();
     }
 
     @Override
     public void setClock(Clock other) {
-
+        time = ((LamportClock) other).getTime();
     }
 
     @Override
@@ -33,11 +32,11 @@ public class LamportClock implements Clock {
 
     @Override
     public boolean happenedBefore(Clock other) {
-        return false;
+        return ((LamportClock) other).getTime()<time;
     }
 
     @Override
     public void setClockFromString(String clock) {
-
+        time = Integer.valueOf(clock);
     }
 }
--- Assignment 3/vs-gruntzp-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/VectorClock.java
+++ Assignment 3/vs-gruntzp-chat/app/src/main/java/ch/ethz/inf/vs/a3/clock/VectorClock.java
@@ -15,7 +15,6 @@ public class VectorClock implements Clock{
     }
 
     void addProcess(Integer pid, int time){
-
     }
 
 
